c4a06146f6af2fc54704b39d20e3f1e45983fee3
Marcos Dias <57465032+SoraAsc@users.noreply.github.com>
2024-11-08 20:01:00 -0300

    feat: added body color and start of pygame configuration

--- celestial_body.py
+++ celestial_body.py
@@ -1,11 +1,31 @@
+from typing import Optional, Tuple
 from numpy import double, ndarray
 import numpy as np
+import random
+
+# Keep track of used colors to avoid duplicates
+used_colors = set()
 
 class CelestialBody:
-    def __init__(self, name: str, mass: double, position: ndarray, velocity: ndarray):
+    def __init__(self, name: str, mass: double, position: ndarray, velocity: ndarray, color: Optional[Tuple[int, int, int]] = None):
         """Initialize the celestial body"""
         self.name = name
         self.mass = mass
         self.position = position
         self.velocity = velocity
         self.acceleration = np.zeros(position.shape)
+        self.color = color if color is not None else self.generate_unique_color()
+
+    def generate_unique_color(self) -> Tuple[int, int, int]:
+        """Generate a unique RGB color that hasn't been used yet"""
+        while True:
+            # Generate a random color
+            new_color = (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
+            if new_color not in used_colors:
+                # Add the color to the used set and return it
+                used_colors.add(new_color)
+                return new_color
+            
+    def __str__(self):
+        """String representation of the object"""
+        return f"CelestialBody(name={self.name}, mass={self.mass}, position={self.position}, velocity={self.velocity}, color={self.color})"
--- main.py
+++ main.py
@@ -1,1 +1,31 @@
 import pygame
+import numpy as np
+
+from celestial_body import CelestialBody
+
+# Pygame Initialization
+pygame.init()
+running = True
+SCREEN_SIZE = np.array([600.0, 600.0])
+screen = pygame.display.set_mode((SCREEN_SIZE[0], SCREEN_SIZE[1]))
+pygame.display.set_caption("Orbit Simulation")
+
+# Constants
+G = 6.67430e-11             # Gravitational constant
+DELTA_T = 800000            # Time interval (adjusted for display)
+SCALE = 3e9                 # Scale to convert meters into pixels (adjusted to reduce values)
+CENTER = SCREEN_SIZE / 2    # Screen center in pixels
+
+earth = CelestialBody("Terra", 0.0, np.array([2, 3]), np.array([2,3]))
+print(earth)
+
+clock = pygame.time.Clock()
+
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+    clock.tick(60)
+
+pygame.quit()
